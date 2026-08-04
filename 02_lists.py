5af6e0665552459aa54d01e9cca820975ce82052
Zenandi Nkujana <zenandinkujana@gmail.com>
2026-08-04 17:46:35 +0200

    Added my lists display.

---
+++ 02_lists.py
@@ -0,0 +1,22 @@
+
+#I am building a movie collection
+#creating a list of movies
+movies = ["The Notebook", "Titanic", "Avatar", "Interstellarr", "John Wick"]
+
+#Display the entire list.
+print(movies) 
+
+#Display how many movies there are.
+print(len(movies))
+
+#Display the first movie.
+print(movies[0])
+
+#Display the last movie.
+print(movies[-1])
+
+#Display the first three movies.
+print(movies[:3])
+
+#Display the last two movies.
+print(movies[-2:])
