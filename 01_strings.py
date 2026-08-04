b045e6e5e1d7a916229b7670610fb9557cd6c201
Zenandi Nkujana <zenandinkujana@gmail.com>
2026-08-04 16:44:17 +0200

    Done with Challenge 1 in Strings.

--- 01_strings.py
+++ 01_strings.py
@@ -1,27 +1,39 @@
-#Challenge 1 - Personal Profile Formatter
 
-#Create a small profile using strings.
-
-#Store the following:
+# ======================================
+# GET USER INFORMATION
+# ======================================
 
 #Full name
 #Favourite movie
-#Favourite programming language
 #Favourite food
 #Favourite city
+#Remove any unwanted spaces at the beginning and end of every user input.
+#The full name is stored with each word starting with a capital letter.
+name = input("Enter your full name: ").strip().title()
+#The favourite movie is stored with each word starting with a capital letter.
+favourite_movie = input("What is your favourite movie? ").strip().title()
+#The favourite food is stored completely in lowercase.
+favourite_food = input("What is your favorite food? ").strip().lower()
+#The favourite city is stored in uppercase.
+favourite_city = input("What is your favourite city? ").strip().upper()
 
-name = input("Enter your full name: ")
-favourite_movie = input("What is your favourite movie? ")
-favourite_food = input("What is your favorite food? ")
-favourite_city = input("What is your favourite city? ")
+# ======================================
+# DISPLAY PROFILE
+# ======================================
 
 #Display them nicely using f-strings.
 print(f"{name}, Your favourite movie to watch is: {favourite_movie}, you love eating: {favourite_food} and your most favorite city is: {favourite_city}. ")
 
+print(F'Thank you, {name}, for completing your profile!')
+
+
+# ======================================
+# STRING CASE CONVERSION
+# ======================================
+
+
 #Print your full name in UPPERCASE.
 print(name.upper())
-
-
 
 #Print your favourite movie in lowercase.
 print(favourite_movie.lower())
@@ -29,24 +41,35 @@ print(favourite_movie.lower())
 #Print your favourite city in Title Case.
 print(favourite_city.title())
 
+# ======================================
+# STRING ANALYSIS
+# ======================================
+
 #Print the length of your full name.
 print(len(name))
 
 #Print only the first name using indexing or slicing.
-print(name.lstrip())
+space = name.find(" ")
+print(name[:space])
 
-#Print the last three letters of your favourite programming language.
-print(favourite_movie[-1:-4])
+#Print the last three letters of your favourite movie.
+print(favourite_movie[-3:])
 
 #Check whether your favourite movie contains the word "the".
-for "the" in favourite_movie:
-    print()
+if "the" in favourite_movie.lower():
+    print(favourite_movie)
+    
+# ======================================
+# STRING MODIFICATION
+# ======================================
 
-#Replace one word in your favourite movie using 
-new_movie = favourite_city.replace("Wuthering", "Rembel")
+
+#Replace one word in your favourite movie.
+new_movie = favourite_movie.replace("Wuthering", "Rembel")
+print(new_movie)
 
 #Replace one word in your favourite movie using .replace().
 greeting = "      Hello Python      "
-greeting.strip()
-
-   
+greeting = greeting.strip()
+print(greeting)
+  
