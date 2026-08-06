28d1b4d452b96ae149c0c76887005b9709205469
Zenandi Nkujana <zenandinkujana@gmail.com>
2026-08-06 16:39:26 +0200

    Done with lists

--- 02_lists.py
+++ 02_lists.py
@@ -3,8 +3,15 @@
 #creating a list of movies
 movies = ["The Notebook", "Titanic", "Avatar", "Interstellarr", "John Wick"]
 
+#======================================
+#LIST DISPLAY
+#======================================
 #Display the entire list.
-print(movies) 
+print(movies)
+
+# ======================================
+# LIST ANALYSIS
+# ====================================== 
 
 #Display how many movies there are.
 print(len(movies))
@@ -20,3 +27,50 @@ print(movies[:3])
 
 #Display the last two movies.
 print(movies[-2:])
+
+# ======================================
+# LIST MODIFICATION
+# ======================================
+
+#Add one new movie.
+movies.append("Wuthering Heights")
+print(movies)
+
+#Insert one movie at a specific position.
+movies.insert(2, "Blood Diamonds")
+print(movies)
+
+#Replace one existing movie.
+movies[0] = "Watermelon & sugar"
+print(movies)
+
+#Remove one movie.
+movies.remove("Titanic")
+print(movies)
+
+#Display the updated list.
+print(movies)
+
+# ======================================
+# LIST SEARCHING
+# ======================================
+
+#Using the same list:
+
+#Check whether "Titanic" exists in the list.
+if "Titanic" in movies:
+    print("Yes the list does have titanic")
+else:
+    print("Titanic was removed")
+#Display the position of "Avatar".
+print(movies.index("Avatar"))
+
+
+# ======================================
+# LIST ORDERING
+# ======================================
+
+#Display the movies in alphabetical order.
+print(sorted(movies))
+#Display them in reverse alphabetical order.
+print(sorted(movies, reverse=True))
