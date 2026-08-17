842a668ce3cea7ebee240dc5948a8a7a9f4ba597
Zenandi Nkujana <zenandinkujana@gmail.com>
2026-08-18 01:59:02 +0200

    Completed some file handling

---
+++ 07_file_handling.py
@@ -0,0 +1,36 @@
+#Movie Watchlist 
+#===========================================
+#READ CONTENTS
+#===========================================
+#I will create a seperate txt file.
+
+#Open the txt file
+file = open("watchlist.txt", "r")
+
+#Reads the contents
+contents = file.read()
+
+#Properly closes the file
+file.close()
+
+#Displays the movies
+print(contents)
+
+#=============================================
+#ADD NEW CONTENT
+#=============================================
+# Add to the watchlist
+file = open("watchlist.txt", "a")
+
+# Add one new movie to the end of the file.
+file.write("\nWuthering Heights")
+
+file.close()
+
+# Read the file again.
+file = open("watchlist.txt", "r")
+contents = file.read()
+file.close()
+
+# Display the updated watchlist.
+print(contents)
